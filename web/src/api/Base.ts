69ae352533a3f030af4d98b1ac330ccb91437c13
Ricardo Balducci <102976117+RicardoBalducci@users.noreply.github.com>
2025-11-23 17:05:48 -0400

    Changes

--- web/src/api/Base.ts
+++ web/src/api/Base.ts
@@ -1,3 +1,3 @@
-export const BASE_API = "http://localhost:3000/"; //
+export const BASE_API = "https://backend-mailliw.onrender.com/"; //
 //https://backend-mailliw.onrender.com/
 //Local http://localhost:3000/
--- web/src/components/global/card/Card.tsx
+++ web/src/components/global/card/Card.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, Typography, Box } from "@mui/material";
-import React, { ReactElement } from "react";
+import  { ReactElement } from "react";
 
 interface DashboardCardProps {
   lineColor?: string;
